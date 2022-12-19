4cc4ff0f049070001721aee784ca6e275c38a83e
VoVanThanh <thanhvo618@gmail.com>
2022-12-19 20:46:40 +0700

    3rd

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('log ne');
+  console.log(process.env.APP_PORT);
+  console.log(process.env.SECRET_KEY);
+  console.log('het log ne');
   res.render('index', { title: 'Express' });
 });
 
